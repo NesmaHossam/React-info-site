025a5b72aacd0cf87834a1478ba342b99e22ac43
NesmaHossam <nh3643769@gmail.com>
2023-11-29 22:49:15 +0200

    added all files in my project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import React from "react";
+
+import Header from "./component/Header";
+import Main from "./component/Main";
+
+
+export default function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Main/>
+    </div>
+  );
+}
+
+// export default App;
